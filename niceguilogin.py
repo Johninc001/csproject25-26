fb18de4a89b81c0acccf869b2fa0c3c889ab50ff
Johninc001 <163934273+Johninc001@users.noreply.github.com>
2025-12-04 02:49:17 +0000

    Add files via upload

--- niceguilogin.py
+++ niceguilogin.py
@@ -1,17 +1,42 @@
 from nicegui import ui
+
 ui.add_head_html('''
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
-''', shared=True)
-ui.add_head_html('''
+<link href="https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,400;500;700&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" />
 ''', shared=True)
-#                            css
-# i have put each item in an individual  ui.add css so it is easier to see
-ui.add_css('''body{}''')
+#                                   css
+#i have put each item in an individual  ui.add css so it is more readable
+ui.add_css('''
+body {
+    background: radial-gradient(circle, skyblue 0%, white 100%);
+    font-family: "DM Sans", sans-serif;
+}
+''', shared=True)
+#functions
+@ui.sub_pages('/other_page')
+def other_page():
+    ui.label('Welcome to the other side')
+def root_page():
+#header
+    ui.sub_pages({ '/': login, '/main_menu': other_page})#ignore
+    with ui.header().classes('bg-gradient-to-r from-blue-500 via-blue-300 to-blue-500 shadow-lg h-20 flex items-center justify-center'):
+        ui.label('login to Greenmount Vet').classes('text-black text-4xl font-normal')
+    
+#this is the main login, currently does't do anything but print the username and password to the console
+def login():
+    with ui.column().classes('w-full h-screen items-center justify-center'):
+        with ui.card().classes('w-96 shadow-xl bg-gray-100 items-center p-8'):
+            ui.label('login').classes('text-2xl mb-4')
+        
+            username = ui.input('username').classes('w-full')
+        
+            ui.separator().classes('w-full border-gray-400 my-4')
+        
+            password = ui.input('password', password=True).classes('w-full')
+        
+            ui.space().classes('h-8')
+        with ui.link('menu', '/other_page').classes('text-blue-600 underline mb-4'):    
+            ui.button('submit', on_click=lambda: print(f'Username: {username.value}, Password: {password.value}')).classes('btn btn-secondary w-40')
 
-with ui.element("div").classes("w3-container w3-red  "):
-    ui.label("login").classes("text-center h-20 w-40")
-    ui.input("username")
-    ui.input("password", password=True)
-    ui.button("submit").classes("btn-secondary w-40")
 ui.run()
